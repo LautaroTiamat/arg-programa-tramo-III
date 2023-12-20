7bb16efdaa56da7d9e054a76c3053712626bce1f
Rolón Lautaro Emanuel <61741642+LautaroTiamat@users.noreply.github.com>
2023-12-20 10:26:01 -0300

    Update servidor.js

--- src/servidor.js
+++ src/servidor.js
@@ -4,13 +4,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
 
-const { database } = require('./config/SequelizeConfig.js');
+/*const { database } = require('./config/SequelizeConfig.js');
 const conectarMongo = require('./config/MongooseConfig.js');
 
 const usuarioRouter = require('./routes/usuarioRoutes.js');
 const autenticacionRouter = require('./routes/autenticacionRoutes.js');
 const archivoRouter = require('./routes/archivoRouter.js');
-const georefRouter = require('./routes/georefRouter.js');
+const georefRouter = require('./routes/georefRouter.js');*/
 
 const app = express();
 const PORT = 3000;
@@ -20,15 +20,19 @@ app.use(bodyParser.json());
 app.use(fileUpload());
 
 // Rutas
-app.use(usuarioRouter);
-app.use(autenticacionRouter);
-app.use(archivoRouter);
-app.use(georefRouter);
+//app.use(usuarioRouter);
+//app.use(autenticacionRouter);
+//app.use(archivoRouter);
+//app.use(georefRouter);
+
+app.get('/', (req, res) => {
+    return res.json({ mensaje: 'El archivo se subió correctamente' });
+})
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
     //database();
-    conectarMongo();
+    //conectarMongo();
 });
 
 module.exports = app;
